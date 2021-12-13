481a34f5f5296d27e983dfd500c0dd76a64be3d6
AntSenokosov <a.y.senokosov@gmail.com>
2021-12-13 17:06:17 +0200

    Add method Result in class Class2

--- Module3HW3/Module3HW3/Class2.cs
+++ Module3HW3/Module3HW3/Class2.cs
@@ -8,5 +8,19 @@ namespace Module3HW3
 {
     public class Class2
     {
+        private Class1 _class1 = new Class1();
+        public bool Result(int x)
+        {
+            var result = _class1.Pow() % x;
+
+            if (result == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
